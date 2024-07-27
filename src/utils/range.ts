f8cc2cb0ee44dca6dd3638a3accd25d04f707109
Vidushi Malik <63915911+Vidushi33@users.noreply.github.com>
2024-07-27 18:20:17 +0530

    modified range function

--- src/utils/range.ts
+++ src/utils/range.ts
@@ -1,9 +1,5 @@
-export function* range(start:number, end?:number){
-    if(end == undefined){
-        end = start
-        start=1
-    }
-
+export function* range(start:number, end?:number=start){
+    if(start==end)start=1
     for(;start <= end; start++){
         yield start;
     }
